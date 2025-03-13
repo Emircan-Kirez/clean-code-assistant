7932dd66617de1d314f0f0319d7d09c4ac9a517b
Emircan Kirez <emircankirezz@gmail.com>
2025-03-13 16:41:57 +0300

    Initial Project

---
+++ src/main/java/dev/emircankirez/clean_code_assistant/CleanCodeAssistantApplication.java
@@ -0,0 +1,13 @@
+package dev.emircankirez.clean_code_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emircankirez/clean_code_assistant/CleanCodeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emircankirez.clean_code_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanCodeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
